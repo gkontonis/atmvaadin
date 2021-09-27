661c403033c398b26806c3af742fa46e29a54ac3
02uz0408 <02uz0408@PVA.SOZVERS.AT>
2021-09-27 14:38:47 +0200

    validator

--- src/main/java/Check.java
+++ src/main/java/Check.java
@@ -1,6 +1,27 @@
 public class Check {
     public boolean validate(String input){
+        if(input == null){
+            return false;
+        }
+        if(input.length() > 9){
+            return false;
+        }
+        if(input.length() <= 0){
+            return false;
+        }
+        if(input.length() == 1){
+            return false;
+        }
 
-        return true;
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+
+        if (input.charAt(input.length() - 1) == 'A' || input.charAt(input.length() - 1) == 'B') {
+            return true;
+        }
+        return false;
     }
 }
