d09b0f13679f56fbfb3363fe145fe9dec243c131
02uz0408 <02uz0408@PVA.SOZVERS.AT>
2021-09-27 14:32:35 +0200

    karlis tests added

---
+++ src/test/java/CurrencyTest/CurrencyTest.java
@@ -0,0 +1,92 @@
+package CurrencyTest;
+
+import org.assertj.core.api.Assertions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class CurrencyTest {
+    // A - Side
+    public void test_currcalc_null() {
+        String testString = null;
+
+        Assertions.assertThat(testString).isNull();
+    }
+
+    public void test_currcalc_empty() {
+        String testString = "";
+    }
+
+    public void test_currcalc_1234A() {
+        String testString = "1234A";
+    }
+
+    public void test_currcalc_6580A() {
+        String testString = "6580A";
+    }
+
+    public void test_currcalc_100A() {
+        String testString = "100A";
+    }
+
+    public void test_currcalc_100_A() {
+        String testString = "100 A";
+    }
+
+    public void test_currcalc_300A() {
+        String testString = "300A";
+    }
+
+    public void test_currcalc_99999A() {
+        String testString = "99999A";
+    }
+
+    public void test_currcalc_Minus_10A() {
+        String testString = "-10A";
+    }
+
+    public void test_currcalc_120Komma5A() {
+        String testString = "120.5A";
+    }
+
+    // B-Side
+    public void test_currcalc_nullB() {
+        String testString = null;
+    }
+
+    public void test_currcalc_emptyB() {
+        String testString = "";
+    }
+
+    public void test_currcalc_1234B() {
+        String testString = "1234B";
+    }
+
+    public void test_currcalc_6580B() {
+        String testString = "6580B";
+    }
+
+    public void test_currcalc_100B() {
+        String testString = "100B";
+    }
+
+    public void test_currcalc_100_B() {
+        String testString = "100 B";
+    }
+
+    public void test_currcalc_300B() {
+        String testString = "300B";
+    }
+
+    public void test_currcalc_99999B() {
+        String testString = "99999B";
+    }
+
+    public void test_currcalc_Minus_10B() {
+        String testString = "-10B";
+    }
+
+    public void test_currcalc_120Komma5B() {
+        String testString = "120.5B";
+    }
+}
