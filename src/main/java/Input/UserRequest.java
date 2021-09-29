ef63338a622036f48d53136085b216dae73387dc
02uz0408 <02uz0408@PVA.SOZVERS.AT>
2021-09-29 11:44:06 +0200

    added UserRequest class and specification

---
+++ src/main/java/Input/UserRequest.java
@@ -0,0 +1,58 @@
+package Input;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserRequest {
+    public static final Set<Character> POSSIBLE_CURRENCIES;
+
+    private final char currencyType;
+    private final int currencyValue;
+
+    static {
+        Set<Character> tmp = new HashSet<Character>();
+        tmp.add('A');
+        tmp.add('B');
+
+        POSSIBLE_CURRENCIES = Collections.unmodifiableSet(tmp);
+    }
+
+    public static boolean isCurrencyTypeValid(char currency) {
+        return POSSIBLE_CURRENCIES.contains(currency);
+    }
+
+    public static boolean isCurrencyValueValid(int currencyValue) {
+        return currencyValue >= 0;
+    }
+
+    public UserRequest(char currencyType, int currencyValue) {
+        validateCurrencyType(currencyType);
+        validateCurrencyValue(currencyValue);
+
+        this.currencyType = currencyType;
+        this.currencyValue = currencyValue;
+    }
+
+    private void validateCurrencyValue(int currencyValue) {
+        if (isCurrencyValueValid(currencyValue)) {
+            return;
+        }
+        throw new IllegalArgumentException("Currency Value has to be >= 0");
+    }
+
+    public char getCurrencyType() {
+        return currencyType;
+    }
+
+    public int getCurrencyValue() {
+        return currencyValue;
+    }
+
+    private void validateCurrencyType(char currencyType) {
+        if (isCurrencyTypeValid(currencyType)) {
+            return;
+        }
+        throw new IllegalArgumentException("Currency Type '" + currencyType + "' is not defined ad valid Currency Type");
+    }
+}
---
+++ src/test/java/Input/UserRequestTest.java
@@ -0,0 +1,60 @@
+package Input;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserRequestTest {
+
+    private final static char INVALID_CURRENCY = '-';
+    @Test
+    public void testGetCurrencyType_It_is_the_set_currency_type() {
+        UserRequest userRequest = new UserRequest('A', 3543);
+        Assert.assertEquals(userRequest.getCurrencyType(), 'A');
+    }
+
+    @Test
+    public void testGetCurrencyType_It_doesnt_thorw_exception_when_currencytype_is_defined_in_Possible_currencies() {
+        UserRequest userRequest = new UserRequest('A', 3543);
+        Assert.assertNotNull(userRequest);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testGetCurrencyType_It_throws_exception_when_currencytype_is_not_defined_in_Possible_currencies() {
+        new UserRequest(INVALID_CURRENCY, 3543);
+    }
+
+    @Test
+    public void testGetCurrencyValue_It_is_the_set_currency_value() {
+        UserRequest userRequest = new UserRequest('A', 3543);
+        Assert.assertEquals(userRequest.getCurrencyValue(), 3543);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testGetCurrencyValue_It_throws_exception_when_currency_type_is_smaller_zero() {
+        new UserRequest('A', -1);
+    }
+
+
+    @Test
+    public void testIsCurrencyValid_It_is_true_when_currency_was_defined_in_possible_currencies() {
+        for (Character currency : UserRequest.POSSIBLE_CURRENCIES) {
+            Assert.assertTrue(UserRequest.isCurrencyTypeValid(currency));
+        }
+    }
+
+    @Test
+    public void testIsCurrencyValid_It_is_false_when_currency_was_not_defined_in_possible_currencies() {
+        Assert.assertFalse(UserRequest.isCurrencyTypeValid(new Character(INVALID_CURRENCY)));
+    }
+
+    @Test
+    public void testIsCurrencyValueValid_It_is_true_when_currencyvalue_greater_equals_zero(){
+        Assert.assertTrue(UserRequest.isCurrencyValueValid(0));
+    }
+    @Test
+    public void testIsCurrencyValueValid_It_is_false_when_currencyvalue_smaller_zero(){
+        Assert.assertFalse(UserRequest.isCurrencyValueValid(-1));
+    }
+
+
+}
