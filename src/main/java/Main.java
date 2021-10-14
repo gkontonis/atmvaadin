47fb33469fa4ccadb182c54f2789f31f4d05ef74
02uz0408 <02uz0408@PVA.SOZVERS.AT>
2021-10-14 14:03:10 +0200

    Added new run testmethods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import input.dto.DepositRequest;
 import input.reader.DepositInputReader;
 import input.reader.PayoutInputReader;
 import input.dto.PayoutRequest;
+import output.MainMenu;
 import output.Output;
 
 public class Main {
@@ -13,29 +14,39 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.run();
+        main.run5();
     }
 
     private void run() {
-
-
-
         PayoutRequest request = INPUT_READER.getUserinput();
-
-        while(request != null) {
-            output.printOutput(calc.calculatePayoutRequest(request));
+        while (request != null) {
+            output.printContainer(calc.calculateSuggestedDenomination(request));
             request = INPUT_READER.getUserinput();
         }
     }
 
-    private void run2(){
-        DepositRequest request = DEPOSIT_INPUT_READER.getUserinput();
-        while(request != null) {
-            output.printOutput(calc.deposit(request));
-            request = DEPOSIT_INPUT_READER.getUserinput();
+   private void run2() {
+       DepositRequest request = DEPOSIT_INPUT_READER.getUserinput();
+       while (request != null) {
+           output.printContainer(calc.deposit(request));
+           request = DEPOSIT_INPUT_READER.getUserinput();
+       }
+   }
+
+    private void run3() {
+        PayoutRequest request = INPUT_READER.getUserinput();
+        while (request != null) {
+            output.printContainer(calc.calculateAndWithdraw(request));
+            request = INPUT_READER.getUserinput();
         }
     }
 
+    private void run4(){
+        output.printContainerB(calc.getContainer());
+    }
 
-
+    private void run5(){
+        MainMenu mainMenu = new MainMenu(calc);
+        mainMenu.showMainMenu();
+    }
 }
