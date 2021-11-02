cbadfb59c0e263d37ea1defbc2665d0ce42d429f
gkontonis <luisxd88@gmail.com>
2021-11-02 15:07:55 +0100

    moneyGrid for website, try

--- src/main/java/frontend/src/main/java/atm/frontend/views/StatusView.java
+++ src/main/java/frontend/src/main/java/atm/frontend/views/StatusView.java
@@ -1,13 +1,20 @@
 package frontend.src.main.java.atm.frontend.views;
 
-import business.src.main.java.atm.business.statusView.StatusViewController;
+import backend.calculator.Calculator;
+import backend.entity.MoneyBox;
+import backend.entity.MoneyBoxContainer;
+import backend.enums.Currency;
+import business.src.main.java.atm.business.statusview.StatusViewController;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.html.Label;
+
+import java.util.List;
 
 
 @Route(value = StatusView.VIEW_ID)
@@ -15,24 +22,27 @@ import com.vaadin.flow.component.html.Label;
 public class StatusView extends VerticalLayout {
     public static final String VIEW_ID = "statusview";
     private StatusViewController controller = new StatusViewController();
+    private Calculator CALCULATOR = new Calculator();
+    private MoneyBoxContainer moneyBoxContainer = new MoneyBoxContainer(); //G
 
-
-    public StatusView(){
+    public StatusView() {
         addClassName("statusView");
         add(label());
         add(statusMenuButtons());
+        add(moneyGrid());
     }
 
-    private VerticalLayout statusMenuButtons(){
+    private VerticalLayout statusMenuButtons() {
         VerticalLayout statusLayout = new VerticalLayout();
         statusLayout.addClassName("statusMenuButtons");
 
-        Text status = new Text(controller.getStatus());
+        //TODO OO Text status = new Text(controller.getStatus());
 
         Button backButton = new Button("Zurück");
         backButton.addClickListener(buttonClickEvent -> UI.getCurrent().navigate(""));
 
-        statusLayout.add(status, backButton);
+        //statusLayout.add(status, backButton);
+        statusLayout.add(backButton);
 
         return statusLayout;
     }
@@ -41,5 +51,36 @@ public class StatusView extends VerticalLayout {
         Label statusLable = new Label("Status");
         statusLable.addClassName("label");
         return statusLable;
+    }
+
+
+    private Grid moneyGrid() {
+        List<MoneyBox> moneyBoxes = null;
+
+        for (Currency currency : CALCULATOR.getContainer().getMap().keySet()) {
+            moneyBoxes = CALCULATOR.getContainer().get(currency);
+        }
+
+        /*for(Currency c : moneyBoxContainer.getMap().keySet()){
+            moneyBoxes = moneyBoxContainer.get(c);
+        }*/
+
+        Grid<MoneyBox> grid = new Grid<>(MoneyBox.class);
+        grid.setItems(moneyBoxes);
+
+
+        grid.removeColumnByKey("value");
+        grid.removeColumnByKey("currency");
+        grid.removeColumnByKey("type");
+        grid.removeColumnByKey("amount");
+
+        grid.addColumn(MoneyBox::getValue).setHeader("Wert");
+        grid.addColumn(MoneyBox::getCurrency).setHeader("Währung");
+        //grid.addColumn(MoneyBox::getType).setHeader("Typ");
+        grid.addColumn(MoneyBox::getAmount).setHeader("Anzahl");
+
+
+        add(grid);
+        return grid;
     }
 }
